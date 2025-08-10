1f34513f90923d14ab6c174edfa288a46c548b27
Jayaswati_P <jayaswatip@gmail.com>
2025-08-10 20:56:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+// client/src/App.js
+
+import React from 'react';
+import AddCustomer from './components/AddCustomer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tailoring Management System</h1>
+      <AddCustomer />
+    </div>
+  );
+}
+
+export default App;
